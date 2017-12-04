fdb1e9efee0fe743f8b0c34da95663417bf95b4f
Nicholas Walczak <nwalczak@bbn.com>
2017-12-04 15:14:41 -0600

    Significant changes to support measurements in um
    The script will now look in the MetaData directory, if it exists, for 'scope
    properties XML files.  It can now read dimension and channel information from
    these XML files.  This includes # of channels, what color the channel
    represents, the # of dimensions, and the dimensions size in # of pixels as well
    as in micrometers.
    
    The size of the pixels is now taken into consideration and output information is
    now in um^2 for area.

--- scripts/cellStats.py
+++ scripts/cellStats.py
@@ -10,8 +10,16 @@ from ij.gui import Roi
 from java.lang import Double
 import os, glob, re, time, sys
 import ConfigParser
-import io
+import xml.etree.ElementTree as ElementTree
 #from jarray import zeros
+
+#Recognized colors
+RED = "Red"
+GREEN = "Green"
+BLUE = "Blue"
+YELLOW = "Yellow"
+BRIGHTFIELD = "Gray"
+SKIP = "Skip"
 
 #
 # Stackoverflow code for numerically sorting strings
@@ -43,17 +51,20 @@ def printUsage():
     
     print "This script will read tif files from an input directory and compute statistics on the cell images."
     print "The script must be pointed to a configuration ini file that will define several important aspects."
+    print "The input and output dirs must be defined in the config file, however all of the rest of the config"
+    print " can be read in from the microscope properties if they exist in the Metadata dir in the input."
     print "The following parameters are recognized in the [Config] section:"
 
     print "inputDir  - Dir to read in tif files from. (Required)"
     print "outputDir - Dir to write output to. (Required)"
-    print "numChannels - Number of channels in dataset, defaults to 4"
-    print "numZ - Number of Z slices in dataset, defaults to 1"
-    print "noZInFile - True if the z slice does not appear in the filename, otherwise false"
-    print "chanLabel - Label that describes source for each channel, default skip, yellow, blue, brightfield"
+    print "numChannels - Number of channels in dataset, defaults to 4, also read from XML properties"
+    print "numZ - Number of Z slices in dataset, defaults to 1, also read from XML properties"
+    print "noZInFile - True if the z slice does not appear in the filename, otherwise false, also read from XML properties"
+    print "chanLabel - Label that describes source for each channel, default skip, yellow, blue, brightfield, also read from XML properties"
     print "            Valid labels: skip, brightfield, red, green, blue, yellow"
-    print "analysisRoi - Rectangular area to perform cell detection on, default 0,0,512,480"
-    print "dsNameIdx - Index of well name within filename, when delimiting on underscores (_)"
+    print "chansToSkip - List of channel names that will be skipped if channels are read from XML properties"
+    print "analysisRoi - Rectangular area to perform cell detection on, default 0,0,512,480, must be defined in config file"
+    print "dsNameIdx - Index of well name within filename, when delimiting on underscores (_), also read from XML properties"
 
     print "Usage: "
     print "<cfgPath>"
@@ -79,39 +90,86 @@ def main(cfg):
     # Sort filenames so they are in order by z and ch
     sort_nicely(imgFiles)
 
-    dsNames = []
+    # Get the names of all wells that exist in this dataset/plate
+    wellNames = []
+    # Each well will have a collection of images, but will all fall under the same common prefix descriptor
+    # Such as: plate1_Aug284pm_A1_S001
+    wellDesc = dict()
+    noZInFile = dict()
+    # Analyze image filenames to get different pieces of information
+    # We care about a time, Z, channel, and the well name
+    timeRE = re.compile("^t[0-9]+$")
+    zRE    = re.compile("z[0-9]+$")
+    chRE   = re.compile("^ch[0-9]+$")
+    wellRE   = re.compile("^[A-Z][0-9]+$")
     for filePath in imgFiles:
-        toks = os.path.basename(filePath).split("_")
-        dsNames.append(toks[cfg.dsNameIdx])
+        fileName = os.path.basename(filePath)
+        toks = os.path.splitext(fileName)[0].split("_")
+        # Parse file name to get indices of certain values
+        tIdx = zIdx = chIdx = sys.maxint
+        wellIndex = sys.maxint # This will be the lowest index that matches the well expression
+        for i in range(0, len(toks)):
+            if timeRE.match(toks[i]):
+                tIdx = i
+            if zRE.match(toks[i]):
+                zIdx = i
+            if chRE.match(toks[i]):
+                chIdx = i
+            if wellRE.match(toks[i]) and i < wellIndex:
+                wellIndex = i
+        
+        minInfoIdx = min(tIdx, min(zIdx, chIdx))
+        wellNames.append(toks[wellIndex])
+        wellDesc[toks[wellIndex]] = fileName[0:fileName.find(toks[minInfoIdx]) - 1]
+        noZInFile[toks[wellIndex]] = zIdx == -1        
 
-    uniqueNames = list(set(dsNames))
+    uniqueNames = list(set(wellNames))
     sort_nicely(uniqueNames)
 
+    # Try to determine pixel size from Leica properties
+    metadataDir = os.path.join(cfg.inputDir, "MetaData")
+    metadataExists = True
+    if not os.path.exists(metadataDir):
+        print "No MetaData directory in input dir! Can't read properties!"
+        metadataExists = False;
+
+    # Process each well
     dsResults = []
-    for datasetName in uniqueNames:
+    for wellName in uniqueNames:
         dsImgFiles = [];
         for i in range(0, len(imgFiles)) :
-            if datasetName == dsNames[i] :
+            if wellName == wellNames[i] :
                 dsImgFiles.append(imgFiles[i])
+        # Make sure output dir exists for well
+        wellPath = os.path.join(cfg.outputDir, wellName)
+        if not os.path.exists(wellPath):
+            os.makedirs(wellPath)
 
-        datasetPath = os.path.join(cfg.outputDir, datasetName)
-        if not os.path.exists(datasetPath):
-            os.makedirs(datasetPath)
-            
+        # Update config based on metadata
+        if (metadataExists):
+            xmlFile = os.path.join(metadataDir, wellDesc[wellName] + "_Properties.xml")
+            if not os.path.exists(xmlFile):
+                print "No metadata XML file for well " + wellName + "! Skipping well.  Path: " + xmlFile
+                continue;
+            updateCfgWithXML(cfg, xmlFile)
+            cfg.noZInFile = noZInFile[wellName] or cfg.numZ == 1
+
+        cfg.printCfg()
         start = time.time()
-        dsResults.append(datasetName + ", " + processDataset(cfg, datasetName, dsImgFiles))
+        dsResults.append(wellName + ", " + processDataset(cfg, wellName, dsImgFiles))
         end = time.time()
-        print("Processed datset " + datasetName + " in " + str(end - start) + " s")
+        print("Processed well " + wellName + " in " + str(end - start) + " s")
 
+    # Write out summary output
     resultsFile = open(os.path.join(cfg.outputDir, "AllResults.csv"), "w")
     
     outputChans = [];
     for chan in cfg.chanLabel:
-        if not chan == "skip" and not chan == "brightfield":
+        if not chan == SKIP and not chan == BRIGHTFIELD:
             outputChans.append(chan)
-    headerString = "frame, brightfield area, "
+    headerString = "frame, brightfield area (um^2), "
     for chan in outputChans:
-        headerString += chan + " area, "
+        headerString += chan + " area (um^2), "
         headerString += "percent " + chan + ", " 
     headerString += "classification \n"
     resultsFile.write(headerString)
@@ -122,7 +180,62 @@ def main(cfg):
 ####
 
 
+####
+#
+# Given a path to a Leica properties XML, read in some configuration
+#
+####
+def updateCfgWithXML(cfg, xmlFile):
+    xmlRoot = ElementTree.parse(xmlFile).getroot()
+    imgEle = xmlRoot.find("Image")
+    imgDescEle = imgEle.find("ImageDescription")
+    
+    # Pull channel info from XML
+    chanEle = imgDescEle.find("Channels")
+    cfg.numChannels = len(chanEle.getchildren())
+    chanNames = []
+    for chan in chanEle.getchildren():
+        newChan = chan.get("LUTName")
+        if newChan in cfg.chansToSkip:
+            chanNames.append(SKIP)
+        else:
+            chanNames.append(chan.get("LUTName"))
+    cfg.chanLabel = chanNames
+    
+    # Pull dimension info from XML 
+    dimsEle = imgDescEle.find("Dimensions")
+    for dimEle in dimsEle.getchildren():
+        numElementsInDim = int(dimEle.get("NumberOfElements"))
+        dimLength = float(dimEle.get("Length"))
+        dimUnit = dimEle.get("Unit")
+        lenMultiplier = getMultiplier(dimUnit) # Ensures length is in micrometers
+        if dimEle.get("DimID") == "X":
+            cfg.pixelWidth = (dimLength * lenMultiplier) / numElementsInDim
+        elif dimEle.get("DimID") == "Y":
+            cfg.pixelHeight = (dimLength * lenMultiplier) / numElementsInDim
+        elif dimEle.get("DimID") == "Z":
+            cfg.numZ = numElementsInDim
+#### 
 
+
+####
+#
+# Get the multiplier to turn the given unit into micrometers
+#
+####
+def getMultiplier(unit):
+    mult = 1;
+    if unit == "m":
+        mult = 1e6;
+    elif unit == "cm":
+        mult = 1e4;
+    elif unit == "mm":
+        mult = 1e3;
+    elif unit == "um":
+        mult = 1;
+    elif unit == "nm":
+        mult = 1e-3;
+    return mult;
 ####
 #
 #
@@ -136,6 +249,7 @@ def processDataset(cfg, datasetName, imgFiles):
 
     # Count how many images we have for each channel/Z slice
     imgFileCats = [[[] for z in range(cfg.numZ)] for c in range(cfg.numChannels)]
+    addedImages = False
     for c in range(0, cfg.numChannels):
         chanStr = 'ch%(channel)02d' % {"channel" : c};
         for z in range(0, cfg.numZ):
@@ -143,8 +257,13 @@ def processDataset(cfg, datasetName, imgFiles):
             for imgPath in imgFiles:
                 fileName = os.path.basename(imgPath)
                 if chanStr in fileName and (cfg.noZInFile or zStr in fileName):
+                    addedImages = True
                     imgFileCats[c][z].append(fileName)
     
+    if not addedImages:
+        print "Failed to add any images to chan/z categories! Problem with input dir?"
+        quit()
+        
     # Load all images
     images = [[0 for z in range(cfg.numZ)] for c in range(cfg.numChannels)]
     for c in range(0, cfg.numChannels):
@@ -168,24 +287,24 @@ def processDataset(cfg, datasetName, imgFiles):
             currIP = images[c][z];
             #currIP.show()
             # We need to get to a grayscale image, which will be done differently for different channels
-            if (cfg.chanLabel[c] == "brightfield"):
+            if (cfg.chanLabel[c] == BRIGHTFIELD):
                 toGray = ImageConverter(currIP)
                 toGray.convertToGray8()
                 minCircularity = 0.02 # We want to identify one big cell ball, so ignore small less circular objects
                 minSize = 40
                 darkBackground = False
-            elif (cfg.chanLabel[c] == "blue") or (cfg.chanLabel[c] == "red") or (cfg.chanLabel[c] == "green"): #
+            elif (cfg.chanLabel[c] == BLUE) or (cfg.chanLabel[c] == RED) or (cfg.chanLabel[c] == GREEN): #
                 chanIdx = 2
-                if (cfg.chanLabel[c] == "red"):
+                if (cfg.chanLabel[c] == RED):
                     chanIdx = 0
-                elif (cfg.chanLabel[c] == "green"):
+                elif (cfg.chanLabel[c] == GREEN):
                     chanIdx = 1;
                 imgChanns = ChannelSplitter.split(currIP);
                 currIP = imgChanns[chanIdx];
                 minCircularity = 0.02
                 minSize = 5
                 darkBackground = True
-            elif (cfg.chanLabel[c] == "yellow"):
+            elif (cfg.chanLabel[c] == YELLOW):
                 title = currIP.getTitle()
                 # Create a new image that consists of the average of the red & green channels
                 width = currIP.getWidth();
@@ -200,7 +319,7 @@ def processDataset(cfg, datasetName, imgFiles):
                 minCircularity = 0.02
                 minSize = 5
                 darkBackground = True
-            elif (cfg.chanLabel[c] == "skip"):
+            elif (cfg.chanLabel[c] == SKIP):
                 areas.append([])
                 continue 
             WindowManager.setTempCurrentImage(currIP);
@@ -239,17 +358,21 @@ def processDataset(cfg, datasetName, imgFiles):
             IJ.saveAs('png', os.path.join(datasetPath, "Segmentation_" + datasetName + "_" + zStr + "_" + chanStr + "_particles.png"))
     
             # The measured areas are listed in the first column of the results table, as a float array:
-            areas.append(table.getColumn(0))
+            newAreas = []
+            if table.getColumn(0):
+                for pixArea in table.getColumn(0):
+                    newAreas.append(pixArea * cfg.pixelHeight * cfg.pixelWidth)
+            areas.append(newAreas)
             #currIP.hide()
         
     resultsFile = open(os.path.join(datasetPath, datasetName + "_results.txt"), "w")
     outputChans = [];
     for chan in cfg.chanLabel:
-        if not chan == "skip" and not chan == "brightfield":
+        if not chan == SKIP and not chan == BRIGHTFIELD:
             outputChans.append(chan)
-    headerString = "frame, brightfield area, "
+    headerString = "frame, brightfield area (um^2), "
     for chan in outputChans:
-        headerString += chan + " area, "
+        headerString += chan + " area (um^2), "
         headerString += "percent " + chan + ", " 
     headerString += "classification \n"
     resultsFile.write(headerString);
@@ -259,7 +382,7 @@ def processDataset(cfg, datasetName, imgFiles):
         area = 0;
         writeArea = False
         # Handle brigthfield channel
-        if (cfg.chanLabel[c] == "brightfield"):
+        if (cfg.chanLabel[c] == BRIGHTFIELD):
             if not areas[c] :
                 area = 0;
             else:
@@ -267,7 +390,7 @@ def processDataset(cfg, datasetName, imgFiles):
                 writeArea = True
             channelAreas["totalArea"] = area
         # Handle Fluorscent Channels   
-        elif (cfg.chanLabel[c] == "blue") or (cfg.chanLabel[c] == "red") or (cfg.chanLabel[c] == "green") or (cfg.chanLabel[c] == "yellow"): #
+        elif (cfg.chanLabel[c] == BLUE) or (cfg.chanLabel[c] == RED) or (cfg.chanLabel[c] == GREEN) or (cfg.chanLabel[c] == YELLOW): #
             if not areas[c] :
                 area = 0;
             else:
@@ -275,20 +398,20 @@ def processDataset(cfg, datasetName, imgFiles):
                 writeArea = True
             channelAreas[cfg.chanLabel[c]] = area
         # Skip channel
-        elif (cfg.chanLabel[c] == "skip"):
+        elif (cfg.chanLabel[c] == SKIP):
             continue
         if writeArea:
             chanResultsFile = open(os.path.join(datasetPath, datasetName + chanStr + "areas.txt"), "w")
             for a in areas[c] :
-                chanResultsFile.write("%d\n" % a)
+                chanResultsFile.write("%10.4f\n" % a)
             chanResultsFile.close()
 
     if channelAreas["totalArea"] == 0:
         channelAreas["totalArea"] = 0.000000001
 
-    resultsString = "%d,\t\t\t %d," % (1, channelAreas["totalArea"])
+    resultsString = "%d,\t\t\t %10.4f," % (1, channelAreas["totalArea"])
     for chan in outputChans:
-        resultsString += "\t\t %d," % channelAreas[chan]
+        resultsString += "\t\t %10.4f," % channelAreas[chan]
         resultsString += "\t\t %0.4f," % (channelAreas[chan] /  channelAreas["totalArea"])
         
     resultsString += "\n"   
@@ -296,12 +419,17 @@ def processDataset(cfg, datasetName, imgFiles):
     resultsFile.close()
     return resultsString
 
+####
+#
+#  The Config Class - storing configuration info
+#
+####
 class config:
     numChannels = 4;
     numZ = 1;
     noZInFile = True;
-    #chanLabel = ['skip', 'brightfield', 'yellow', 'blue'];
-    chanLabel = ['skip', 'yellow', 'blue', 'brightfield'];
+    chanLabel = [SKIP, YELLOW, BLUE, BRIGHTFIELD];
+    chansToSkip = [];
     inputDir = '';
     outputDir = '';
     # We need to avoid the scale bar in the bottom of the image, so set a roi that doesn't include it
@@ -309,6 +437,25 @@ class config:
     analysisRoi = Roi(0,0,1024,980)
     # Determines what index the dataset name is within the tokenized filename
     dsNameIdx = 4;
+    pixelHeight = 1; # in micrometers
+    pixelWidth = 1; # in micrometers
+    
+    def printCfg(self):
+        print("Using Config:")
+        print("\tinputDir:\t"    + self.inputDir)
+        print("\toutputDir:\t"   + self.outputDir)
+        print("\tnumChannels:\t" + str(self.numChannels))
+        print("\tnumZ:\t\t"        + str(self.numZ))
+        print("\tnoZInFile:\t"   + str(self.noZInFile))
+        print("\tchanLabel:\t"   + ", ".join(self.chanLabel))
+        print("\tanalysisRoi:\t" + str(self.analysisRoi))
+        print("\tpixelHeight:\t" + str(self.pixelHeight))
+        print("\tpixelWidth:\t" + str(self.pixelWidth))
+        print("\n")
+####
+#
+#
+####
     
     
 ####
@@ -356,6 +503,11 @@ for option in cfgParser.options("Config"):
         cfg.numZ = int(cfgParser.get("Config", option))
     elif option == "nozinfile":
         cfg.noZInFile  = bool(cfgParser.get("Config", option))
+    elif option == "chanstoskip":
+        toks = cfgParser.get("Config", option).split(",")
+        cfg.chansToSkip = []
+        for tok in toks:
+            cfg.chansToSkip.append(tok.strip())
     elif option == "chanlabel":
         toks = cfgParser.get("Config", option).split(",")
         if not len(toks) == numChannels:
@@ -377,11 +529,8 @@ for option in cfgParser.options("Config"):
 print("Using Config:")
 print("\tinputDir:\t"    + cfg.inputDir)
 print("\toutputDir:\t"   + cfg.outputDir)
-print("\tnumChannels:\t" + str(cfg.numChannels))
-print("\tnumZ:\t\t"        + str(cfg.numZ))
-print("\tnoZInFile:\t"   + str(cfg.noZInFile))
-print("\tchanLabel:\t"   + ", ".join(cfg.chanLabel))
 print("\tanalysisRoi:\t" + str(cfg.analysisRoi))
+print("\tchansToSkip:\t" + ", ".join(cfg.chansToSkip))
 print("\n")
 
 
