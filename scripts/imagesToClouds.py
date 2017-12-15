9d36a8932e0b3d0037a323cd256c866b9eb13b4d
Nicholas Walczak <nwalczak@bbn.com>
2017-12-15 14:01:30 -0600

    System path fix

--- scripts/imagesToClouds.py
+++ scripts/imagesToClouds.py
@@ -6,6 +6,11 @@ from ij.plugin import ChannelSplitter, ImageCalculator
 from java.lang import Double
 import os, glob, re, time, sys
 
+# I'm not certain why, but when run in ImageJ it doesn't seem to adhere to the CLASSPATH env variable
+# This ensures that CLASSPATH is explicitly on the module search path, which is required for ELMConfig to resolve
+for path in os.environ['CLASSPATH'].split(os.pathsep):
+    sys.path.append(path)
+
 import ELMConfig
     
 #
