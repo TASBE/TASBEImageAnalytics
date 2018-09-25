83a2eabeda243cb1dce545ab2a131d348b7f1e87
Nicholas Walczak <nwalczak@bbn.com>
2018-09-25 08:13:26 -0500

    Added ability to exclude specific wells

--- scripts/cellStats.py
+++ scripts/cellStats.py
@@ -220,6 +220,9 @@ def main(cfg):
         if cfg.getValue(ELMConfig.wellNames):
             if not any(wellName in name for name in cfg.getValue(ELMConfig.wellNames)):
                 continue;
+        if cfg.getValue(ELMConfig.excludeWellNames):
+            if any(wellName in name for name in cfg.getValue(ELMConfig.excludeWellNames)):
+                continue;
 
         # Get files in this well
         dsImgFiles = [];
